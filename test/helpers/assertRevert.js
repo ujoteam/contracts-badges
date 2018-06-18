7058efa473a4b0d91c74736427f728d1da00b197
Simon de la Rouviere <simon@delarouviere.com>
2018-06-18 23:10:05 +0200

    add error message to asserts

--- test/helpers/assertRevert.js
+++ test/helpers/assertRevert.js
@@ -4,6 +4,7 @@ module.exports = {
       await promise;
       assert.fail('Expected revert not received');
     } catch (error) {
+      console.log(error.message);
       const revertFound = error.message.search('revert') >= 0;
       assert(revertFound, `Expected "revert", got ${error} instead`);
     }
