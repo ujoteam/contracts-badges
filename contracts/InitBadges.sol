0ec575bc648c2dc1fa0951581df7c4f4e6d242f7
Simon de la Rouviere <simon@delarouviere.com>
2018-08-23 15:30:31 +0200

    wip on badges refactor

---
+++ contracts/InitBadges.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+
+/*
+This re-adds badges that were minted in:
+https://etherscan.io/address/0xc45e027f0f9d7e90e612be02d4e710a632a9dba9
+Up to:
+https://etherscan.io/tx/0x6d021d4aedea8404ae4bc5514d702341494a254e4b784bf7a20f0574ed497f07
+
+12 badges
+*/
+
+library InitBadges {
+    function initialise() {
+        // function createBadge(address _buyer, string _mgCid, string _nftCid, address _beneficiary, uint256 _usdCost)
+        // todo:
+        // createBadge
+        // createBadge
+    }
+}
